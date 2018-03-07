b2cb4285599ac1613fb8f3b63c73674e955b2bdb
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-03-07 16:42:55 -0500

    created a movies componenet and populated some jsx to start off

---
+++ src/Movie.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+  render() {
+    return (
+      // This is where we add our JSX
+      <div>
+        <h1>The Lord of the Rings: A Trilogy</h1>
+        <p>4h 37min</p>
+      </div>
+    )
+  }
+}
+
+export default Movie;
